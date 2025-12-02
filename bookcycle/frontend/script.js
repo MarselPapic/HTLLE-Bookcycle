df8b5e0fae9d3f6b4f0824ffdb4b380c64019a51
papic <papicmarsel3gg@@gmail.com>
2025-12-02 15:19:16 +0100

    Second commit with first files

---
+++ bookcycle/frontend/script.js
@@ -0,0 +1,1 @@
+console.log('Bookcycle frontend loaded');
---
+++ bookcycle/server/src/main/java/com/bookcycle/BookcycleApplication.java
@@ -0,0 +1,11 @@
+package com.bookcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcycleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookcycleApplication.class, args);
+    }
+}
